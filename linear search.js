92714fc941c2cdeda593f624634840039906443c
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2023-10-01 04:25:03 +0600

    linear search

---
+++ linear search.js
@@ -0,0 +1,17 @@
+const arr = [1, 5, 3, 4, 8];
+var flag = 0;
+var num = 10
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == num) {
+        flag = 1;
+    }
+}
+
+if (flag == 1) {
+
+    console.log("Value Found");
+} else {
+
+    console.log("Value not found");
+}

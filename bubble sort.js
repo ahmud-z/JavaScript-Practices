26a728957a4b377362f9a322421a0a2f6e194884
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2024-01-13 22:43:00 +0600

    bubble sort

---
+++ bubble sort.js
@@ -0,0 +1,26 @@
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] < arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+
+function main() {
+
+    let arr = [1, 54, 12, 3, 6, 5, 2, 0];
+    bubbleSort(arr);
+
+    console.log('Sorted Array elements: ');
+
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+    }
+}
+
+main();

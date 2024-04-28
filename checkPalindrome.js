36f7b8bbcd0725c0360339068afdce730d96b6a4
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2024-04-28 08:32:07 +0600

    find palindrome

---
+++ checkPalindrome.js
@@ -0,0 +1,24 @@
+function checkPalindrome(num) {
+    let temp = num;
+    let reverseNum = 0;
+
+    while (temp != 0) {
+        let remainder = temp % 10;
+
+        reverseNum = (reverseNum * 10) + remainder;
+
+        temp = Math.floor(temp / 10);
+    }
+
+    if(reverseNum == num){
+        console.log('Palindrome');
+    }else{
+        console.log('Not Palindrome');
+    }
+}
+
+function main() {
+    checkPalindrome(105);
+}
+
+main();

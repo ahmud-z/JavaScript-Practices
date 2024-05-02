19948864afcb11aa055fdbeb2859bc11c97365a0
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2024-05-02 09:05:22 +0600

    check leap year

---
+++ checkLeapYear.js
@@ -0,0 +1,15 @@
+function checkLeapYear(year) {
+    if ((year % 4 == 0) && ((year % 400 == 0)
+        || (year % 100 !== 0))) {
+        console.log(year + ' is a leap year');
+    } else {
+        console.log(year + ' is not a leap year');
+    }
+}
+
+function main() {
+    let year = prompt('Enter a year: ');
+    checkLeapYear(year);
+}
+
+main();
